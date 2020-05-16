9f3a457112712c091c1e867596b57bf15f0eefac
Areebalicoder <63393212+Areebalicoder@users.noreply.github.com>
2020-05-16 13:59:42 +0530

    Add files via upload

---
+++ AngryBirdsStage7-master/Pig.js
@@ -0,0 +1,28 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,100,50);
+    this.image = loadImage("sprites/pig.png");
+    this.Visiblity = 255;
+  }
+
+ display(){
+   //console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+    }
+  }
+
+  score(){
+    if(bird.Body.isTouching(pig.body)){
+      score = score + 100;
+  }
+  }
+};
